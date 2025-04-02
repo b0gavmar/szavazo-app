c4c0c452e93744be7d66ecc790e2fc5ecefbfd9b
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-04-02 14:19:21 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+        <nav class="text-center">
+        <RouterLink class="btn btn-primary m-3" to="/">szavazok</RouterLink>
+        <RouterLink class="btn btn-primary" to="/eredmeny">Eredmeny</RouterLink>
+      </nav>
+    </div>
+
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/SzavazoView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Itt szavazhatsz!</h1>
+  <select v-model="temak">
+    <option v-for="n in 6" value="">{{ n }}</option>
+  </select>
+</template>
