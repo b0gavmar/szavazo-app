aec8e9035bfe91b8a9a2595be76e3f3e2f4e27db
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-04-02 14:51:32 +0200

    ujranevezes

--- src/stores/szavazatok.js
+++ src/stores/szavazatok.js
@@ -2,14 +2,14 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
-export const useTemaStore = defineStore('tema', () => {
+export const useSzavazatokStore = defineStore('szavazatok', () => {
   const temak = ref([])
 
-  const temaFeltoltes = async () => {
+  const loadTemak = async () => {
     const resp = await axios.get("http://localhost:3000/temak")
     temak.value = resp.data
   }
 
 
-  return { temak, temaFeltoltes }
+  return { temak, loadTemak }
 })
--- src/views/SzavazoView.vue
+++ src/views/SzavazoView.vue
@@ -1,17 +1,19 @@
 <script setup>
 import { onMounted } from 'vue';
-import { useTemaStore} from '@/stores/tema';
-const temaStore = useTemaStore();
+import { useSzavazatokStore} from '@/stores/szavazatok';
+const szavazatokStore = useSzavazatokStore();
 
 onMounted(async ()=>{
-  await temaStore.temaFeltoltes();
+  await szavazatokStore.loadTemak();
 })
 
 </script>
 
 <template>
   <h1>Itt szavazhatsz!</h1>
+  <p> Válassz témát
   <select v-model="temak">
-    <option v-for="tema in temaStore.temak" value="">{{ tema.tema }}</option>
+    <option v-for="tema in szavazatokStore.temak" :value="tema.id">{{ tema.tema }}</option>
   </select>
+  </p>
 </template>
