0088b110483f05fe87b5794a3a06e9d6dc54fb0f
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-04-04 08:42:27 +0200

    kiuriti az opciokat

--- src/stores/szavazatok.js
+++ src/stores/szavazatok.js
@@ -1,20 +1,24 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-import axios from 'axios'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import axios from "axios";
 
-export const useSzavazatokStore = defineStore('szavazatok', () => {
-  const temak = ref([])
-  const opciok = ref([])
+export const useSzavazatokStore = defineStore("szavazatok", () => {
+  const temak = ref([]);
+  const opciok = ref([]);
 
   const loadTemak = async () => {
-    const resp = await axios.get("http://localhost:3000/temak")
-    temak.value = resp.data
-  }
+    const resp = await axios.get("http://localhost:3000/temak");
+    temak.value = resp.data;
+  };
 
   const loadOpciok = async (id) => {
-    const resp = await axios.get("http://localhost:3000/opciok")
-    opciok.value = resp.data.filter(o => o.temId == id)
-  }
+    const resp = await axios.get("http://localhost:3000/opciok");
+    opciok.value = resp.data.filter((o) => o.temId == id);
+  };
 
-  return { temak, opciok, loadTemak, loadOpciok }
-})
+  const szavazok = async (id) => {
+    console.log(id);
+  };
+
+  return { temak, opciok, loadTemak, loadOpciok, szavazok };
+});
--- src/views/SzavazoView.vue
+++ src/views/SzavazoView.vue
@@ -1,23 +1,31 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import { useSzavazatokStore} from '@/stores/szavazatok';
+import { onMounted, ref } from "vue";
+import { useSzavazatokStore } from "@/stores/szavazatok";
 const szavazatokStore = useSzavazatokStore();
 const temaId = ref(null);
+const szavazat = ref()
 
-onMounted(async ()=>{
+onMounted(async () => {
   await szavazatokStore.loadTemak();
-})
-
+  await szavazatokStore.loadOpciok();
+});
 </script>
 
 <template>
   <h1>Itt szavazhatsz!</h1>
-  <p> Válassz témát
-  <select v-model="temaId" @change="storeToRefs.loadOpciok(temaId)">
-    <option v-for="tema in szavazatokStore.temak" :value="tema.id">{{ tema.tema }}</option>
-  </select>
+  <p>
+    Válassz témát
+    <select v-model="temaId" @change="szavazatokStore.loadOpciok(temaId)">
+      <option v-for="tema in szavazatokStore.temak" :value="tema.id">
+        {{ tema.tema }}
+      </option>
+    </select>
   </p>
   <div v-if="temaId">
-    <h3></h3>
+    <h3>Szavazati lehetőségek:</h3>
+    <p v-for="o in szavazatokStore.opciok">
+      <input type="radio" v-model="szavazat" :value="o.id" >{{ o.opcio }}<input>
+    </p>
+    <button @click="szavazatokStore.szavazok(szavazat)" class="btn btn-danger">Szavazok</button>
   </div>
 </template>
